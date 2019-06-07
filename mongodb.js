126477c7f029c9441b0a021ff346f0db798e4d80
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-07 10:35:07 +0300

    successfully added a new entry to the db --> all is working fine!

--- mongodb.js
+++ mongodb.js
@@ -6,4 +6,6 @@ const connectionUrl = 'mongodb://127.0.0.1:27017',
 MongoClient.connect(connectionUrl,{useNewUrlParser:true}, (error, client)=>{
   if (error) return console.log('unable to connect to the db');
   console.log(`Connected to the following db: ${dbName} @ address ${connectionUrl}`);
+  const db = client.db(dbName);
+  db.collection('users').insertOne({username:'PIY', name:'Peter Yonkov'});
 });
