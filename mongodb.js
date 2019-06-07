23e04a686540d457a01b55b279184b2992bf90e6
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-07 11:05:11 +0300

    mongo search for a doc...

--- mongodb.js
+++ mongodb.js
@@ -7,19 +7,10 @@ MongoClient.connect(connectionUrl,{useNewUrlParser:true}, (error, client)=>{
   if (error) return console.log('unable to connect to the db');
   console.log(`Connected to the following db: ${dbName} @ address ${connectionUrl}`);
   const db = client.db(dbName);
-  // db.collection('users').insertOne({username:'PIY', name:'Peter Yonkov'},(err, res)=>{
-  //   if (error) return console.log('unable to add a user to the db :(((');
-  //   console.log(res.ops);
-  // });
-  const usersToAdd = [
-    {username:'Andrea', name:'what'},
-    {username:'Jen', name:'ever'},
-    {username:'Pepe', name:'it takes'},
-    {username:'Joe', name:'to be a happy camper'}
-  ];
-  db.collection('users').insertMany(usersToAdd,(error, res)=>{
-      if (error) return console.log('unable to add a user to the db :(((');
-      console.log(res.ops);
+
+  db.collection('users').findOne({username:'PIY'}, (err, res) =>{
+    if (err) return console.log('unable to get the user you wanted...');
+    console.log(res);
   });
 
 });
