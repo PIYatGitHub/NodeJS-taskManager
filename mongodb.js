f544c86878c94982b3f341acaf7e1833d64eb3f5
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-07 10:31:31 +0300

    now connecting to the mongodb server @ 127.0.0.1

---
+++ mongodb.js
@@ -0,0 +1,9 @@
+const mongo = require('mongodb'),
+      MongoClient = mongo.MongoClient;
+const connectionUrl = 'mongodb://127.0.0.1:27017',
+      dbName = 'taskManager';
+
+MongoClient.connect(connectionUrl,{useNewUrlParser:true}, (error, client)=>{
+  if (error) return console.log('unable to connect to the db');
+  console.log(`Connected to the following db: ${dbName} @ address ${connectionUrl}`);
+});
