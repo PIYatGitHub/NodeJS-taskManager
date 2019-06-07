e0a4f5d480ece666935a9dade50550587285ea4d
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-07 18:53:57 +0300

    now able to get all users and all tasks; r single user/task by _id

--- src/index.js
+++ src/index.js
@@ -9,10 +9,46 @@ const User = require('./models/user'),
 
 app.use(express.json());
 
+app.get('/users', (req, res)=>{
+  User.find({}).then((users)=>{
+    res.send(users)
+  }).catch(()=>{
+    res.status(500).send();
+  });
+});
+
+app.get('/users/:id', (req, res)=>{
+  User.findById({_id:req.params.id}).then((user)=>{
+    if (!user) res.status(404).send();
+    res.send(user);
+  }).catch(()=>{
+    res.status(500).send();
+  });
+});
+
 app.post('/users', (req, res)=>{
   const user = new User(req.body);
-  user.save().then(()=>{res.status(201).send(user)}).catch((err)=>{
+  user.save().then(()=>{
+    res.status(201).send(user)
+  }).catch((err)=>{
     res.status(400).send(err);
+  });
+});
+
+app.get('/tasks', (req, res)=>{
+  Task.find({}).then((tasks)=>{
+    res.send(tasks)
+  }).catch(()=>{
+    res.status(500).send();
+  });
+});
+
+app.get('/tasks/:id', (req, res)=>{
+  Task.findById({_id:req.params.id}).then((task)=>{
+    if (!task) res.status(404).send();
+    res.send(task);
+  }).catch(()=>{
+    res.status(500).send();
   });
 });
 
