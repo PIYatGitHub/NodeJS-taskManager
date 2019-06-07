3c8a837d248ffd786e9b2b38a76cc3b39b30fc5b
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-07 18:33:19 +0300

    creating tasks now works; statuses for both requests changed to 201

--- src/index.js
+++ src/index.js
@@ -11,9 +11,15 @@ app.use(express.json());
 
 app.post('/users', (req, res)=>{
   const user = new User(req.body);
-  user.save().then(()=>{res.send(user)}).catch((err)=>{
+  user.save().then(()=>{res.status(201).send(user)}).catch((err)=>{
     res.status(400).send(err);
   });
 });
 
+app.post('/tasks', (req, res)=>{
+  const task = new Task(req.body);
+  task.save().then(()=>{res.status(201).send(task)}).catch((err)=>{
+    res.status(400).send(err);
+  });
+});
 app.listen(port,()=>console.log(`server is up on ${port}`));
