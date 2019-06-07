60035e1c86cf5cd64343cfef71e4e53f2c3a7901
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-07 16:03:21 +0300

    email and age now validated...

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'),
-      connectionURL = 'mongodb://127.0.0.1:27017/taskManager',
+      validator = require('validator');
+const connectionURL = 'mongodb://127.0.0.1:27017/taskManager',
       dbOptions= {useNewUrlParser:true, useCreateIndex:true};
 mongoose.connect(connectionURL, dbOptions);
 
@@ -7,9 +8,20 @@ const User = mongoose.model('User',{
     name:{
       type:String
     },
+    email:{
+      type:String,
+      required:true,
+      validate(value){
+        if(!validator.isEmail(value)) throw new Error('Invalid email...')
+      }
+    },
     age:{
       type:Number,
-      required:true
+      required:true,
+      validate(value){
+        if(value<13) throw new Error('Get back to the analogue world, will you!');
+        if(value>130) throw new Error('You seem too old to have a PC. Enter some realistic age...!');
+      }
     }
 });
 
@@ -26,13 +38,14 @@ const Task = mongoose.model('Task',{
   }
 });
 
-// const me = new User({
-//   name:"Petar Iv. Yonkov",
-//   age: 27
-// });
-// me.save().then(()=>console.log('did it go in -- yes!')).catch((err)=>{console.log(err)});
-
-const task2 = new Task({
-  complete: true
+const me = new User({
+  name:"Petar Iv. Yonkov",
+  age: 108,
+  email:'me@1.com'
 });
-task2.save().then(()=>console.log('did it go in -- yes!')).catch((err)=>{console.log(err)});
+me.save().then(()=>console.log('did it go in -- yes!')).catch((err)=>{console.log(err)});
+
+// const task2 = new Task({
+//   complete: true
+// });
+// task2.save().then(()=>console.log('did it go in -- yes!')).catch((err)=>{console.log(err)});
