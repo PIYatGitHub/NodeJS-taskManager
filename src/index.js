9d5b1feffb4724064f0aa14dbdbd697a4962cbb3
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-08 09:08:45 +0300

    added a delete operation for the task

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ app.delete('/users/:id', async (req, res)=>{
     if(!user) return res.status(404).send();
     res.send(user)
   }catch (e) {
-    res.status(400).send(e)
+    res.status(500).send(e)
   }
 });
 
@@ -100,11 +100,21 @@ app.patch('/tasks/:id', async (req, res)=>{
   }
 
   try{
-    const task = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true});
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true});
     if(!task) return res.status(404).send();
     res.send(task)
   }catch (e) {
     res.status(400).send(e)
+  }
+});
+
+app.delete('/tasks/:id', async (req, res)=>{
+  try{
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if(!task) return res.status(404).send();
+    res.send(task)
+  }catch (e) {
+    res.status(500).send(e)
   }
 });
 
