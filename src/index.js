d21d287fd8b9ebd0a536c3c9082903c6df6e79e4
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-08 09:27:55 +0300

    prettify index.js --now only 10 rows

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-const express = require('express');
 require('./db/mongoose');
-const middleware = require('./middleware');
-
-const app =express(),
-      port = process.env.PORT||3000;
-
+const express = require('express');
+const app =express(), port = process.env.PORT||3000;
 const userRouter = require('./routers/user'),
       taskRouter = require('./routers/task');
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
 app.listen(port,()=>console.log(`server is up on ${port}`));
