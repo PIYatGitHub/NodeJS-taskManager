8f47dfa74a14ac99dde2d43599a15a4bd461081e
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-08 09:20:32 +0300

    grans CRUD refactor -- separate routers -- part 1 -- the user router

--- src/index.js
+++ src/index.js
@@ -5,64 +5,12 @@ const middleware = require('./middleware');
 const app =express(),
       port = process.env.PORT||3000;
 
-const User = require('./models/user'),
+const userRouter = require('./routers/user'),
       Task = require('./models/task');
 
 app.use(express.json());
+app.use(userRouter);
 
-app.get('/users', async (req, res)=>{
-  try {
-    const users = await User.find({});
-    res.send(users)
-  }catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get('/users/:id', async (req, res)=>{
-  try {
-    const user = await User.findById({_id:req.params.id});
-    if (!user) return res.status(404).send();
-    res.send(user);
-  }catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.post('/users', async (req, res)=>{
-  const user = new User(req.body);
-  try{
-    await user.save();
-    res.status(201).send(user)
-  }catch (e) {
-    res.status(400).send(e)
-  }
-});
-
-app.patch('/users/:id', async (req, res)=>{
-  const allowedUpdates = ['name', 'age', 'email', 'password'];
-  if (!middleware.validateUpdate(req.body, allowedUpdates)){
-    return res.status(400).send({error:'Invalid updates!'});
-  }
-
-  try{
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true});
-    if(!user) return res.status(404).send();
-    res.send(user)
-  }catch (e) {
-    res.status(400).send(e)
-  }
-});
-
-app.delete('/users/:id', async (req, res)=>{
-  try{
-    const user = await User.findByIdAndDelete(req.params.id);
-    if(!user) return res.status(404).send();
-    res.send(user)
-  }catch (e) {
-    res.status(500).send(e)
-  }
-});
 
 app.get('/tasks', async (req, res)=>{
   try {
