d3749e95bc8b4d88855e857096c98f5f8e35396e
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-08 10:07:32 +0300

    task router changed!

--- src/models/user.js
+++ src/models/user.js
@@ -35,7 +35,6 @@ const userSchema = new mongoose.Schema({
 });
 userSchema.pre('save', async function (next) {
   const user = this;
-  console.log('in here???');
   if (user.isModified('password')) user.password = await bcrypt.hash(user.password,8);
   next();
 });
