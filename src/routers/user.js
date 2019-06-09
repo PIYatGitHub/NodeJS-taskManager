f0a828c6369f37fc209df66d40d835ec33991ff8
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-09 15:06:51 +0300

    deleting avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -101,7 +101,7 @@ router.delete('/users/me/avatar', middleware.auth, async (req, res)=>{
   try{
     req.user.avatar = undefined;
     await req.user.save();
-    res.status(200).send();
+    res.send();
   }catch (e) {
     res.status(500).send(e)
   }
