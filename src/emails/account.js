5d2309392925a78b5b10a5f53d2f1611c14cdc64
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-09 23:18:49 +0300

    setting up the hello email

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,9 +2,14 @@ const sgMail = require('@sendgrid/mail'),
       config = require('../utils/config');
 sgMail.setApiKey(config.email_api_key);
 
-sgMail.send({
-  to:'10vpetyr.yonkov@gmail.com',
-  from: '10vpetyr.yonkov@gmail.com',
-  subject: '@Echo',
-  text: 'does it send to you???'
-});
+const sendWelcomeEmail = (email, name)=>{
+  sgMail.send({
+    to: email,
+    from: '10vpetyr.yonkov@gmail.com',
+    subject: `Hello, ${name}! Welcome aboard.`,
+    text: 'We are glad you are joining us!'
+  });
+};
+module.exports = {
+  sendWelcomeEmail
+};
