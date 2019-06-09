b41fa51c08e43c8d0f8a1b6b102efee8632f590d
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-09 10:06:03 +0300

    tasks are almost done now...

--- src/routers/task.js
+++ src/routers/task.js
@@ -49,9 +49,9 @@ router.patch('/tasks/:id',middleware.auth, async (req, res)=>{
   }
 });
 
-router.delete('/tasks/:id', async (req, res)=>{
+router.delete('/tasks/:id',middleware.auth, async (req, res)=>{
   try{
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({_id:req.params.id, owner:req.user._id});
     if(!task) return res.status(404).send();
     res.send(task)
   }catch (e) {
