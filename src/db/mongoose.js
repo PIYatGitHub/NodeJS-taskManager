eeb27c8233e59937106f96f9f7b321a11b4cada4
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-09 23:39:41 +0300

    setting up env variables

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+
 const connectionURL = 'mongodb://127.0.0.1:27017/taskManager',
-      dbOptions= {useNewUrlParser:true, useCreateIndex:true};
+      dbOptions= {
+          useNewUrlParser:true,
+          useCreateIndex:true
+      };
+
 mongoose.connect(connectionURL, dbOptions);
