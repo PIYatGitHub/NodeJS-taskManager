a320e8ef922a527236b81665079fd11c24ebc407
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-10 13:13:17 +0300

    wiped the DB...created a test user and added a login test for good data

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,18 @@
 const request = require('supertest'),
-      app = require('../src/app');
+      app = require('../src/app'),
+      User = require('../src/models/user');
+
+const testUser_1 = {
+  name:"I am a test user",
+  email:"test@test.com",
+  password:"SerbiaStrong",
+  age: 28
+};
+
+beforeEach(async ()=>{
+  await User.deleteMany();
+  await new User(testUser_1).save();
+});
 
 test('Should signup a new user', async()=>{
     await request(app).post('/users').send(
@@ -7,7 +20,15 @@ test('Should signup a new user', async()=>{
         name:"Petar Yonkov",
         email:"10vpetyryonkov@gmail.com",
         password:"SerbiaStrong",
-        age: 27
+        age: 33
       }
     ).expect(201)
+});
+
+
+test('Should login the existing test user', async()=>{
+ await request(app).post('/users/login').send({
+   email: testUser_1.email,
+   password:testUser_1.password
+ }).expect(200)
 });
