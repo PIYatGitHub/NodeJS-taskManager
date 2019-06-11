25b10b927ff6e83fe4909acc50d3302bb40811eb
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-06-11 08:00:48 +0300

    added support for better task tests, made a few more tests for the tasks, now totaling in 3

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,10 +1,11 @@
 const mongoose= require ('mongoose'),
       jwt = require ('jsonwebtoken'),
-      User = require('../../src/models/user');
+      User = require('../../src/models/user'),
+      Task = require('../../src/models/task');
 
 
-const testUser_1_id = new mongoose.Types.ObjectId();
-const testUser_1 = {
+const testUser_1_id = new mongoose.Types.ObjectId(),
+      testUser_1 = {
   _id: testUser_1_id,
   name:"I am a test user",
   email:"test@test.com",
@@ -15,13 +16,73 @@ const testUser_1 = {
   }]
 };
 
+const testUser_2_id = new mongoose.Types.ObjectId(),
+      testUser_2 = {
+  _id: testUser_2_id,
+  name:"Second test user",
+  email:"test2@test.com",
+  password:"SerbiaStrong",
+  age: 45,
+  tokens:[{
+    token: jwt.sign({_id:testUser_2_id}, process.env.JWT_SECRET)
+  }]
+};
+
+const task_1 = {
+  _id:mongoose.Types.ObjectId(),
+  description:"A test task # 1",
+  complete: true,
+  owner: testUser_1_id
+};
+const task_2 = {
+  _id:mongoose.Types.ObjectId(),
+  description:"A test task # 2",
+  complete: true,
+  owner: testUser_1_id
+};
+const task_3 = {
+  _id:mongoose.Types.ObjectId(),
+  description:"A test task # 3",
+  complete: false,
+  owner: testUser_1_id
+};
+const task_4 = {
+  _id:mongoose.Types.ObjectId(),
+  description:"A test task # 4",
+  complete: false,
+  owner: testUser_1_id
+};
+const task_5 = {
+  _id:mongoose.Types.ObjectId(),
+  description:"A test task # 5",
+  complete: false,
+  owner: testUser_2_id
+};
+const task_6 = {
+  _id:mongoose.Types.ObjectId(),
+  description:"A test task # 6",
+  complete: true,
+  owner: testUser_2_id
+};
+
 const setupDB = async ()=>{
   await User.deleteMany();
   await new User(testUser_1).save();
+  await new User(testUser_2).save();
+  await Task.deleteMany();
+  await new Task(task_1).save();
+  await new Task(task_2).save();
+  await new Task(task_3).save();
+  await new Task(task_4).save();
+  await new Task(task_5).save();
+  await new Task(task_6).save();
 };
 
 module.exports={
-testUser_1_id,
-  testUser_1,
+  testUser_1_id,  testUser_2_id,
+  testUser_1,     testUser_2,
+  task_1,         task_2,
+  task_3,         task_4,
+  task_5,         task_6,
   setupDB
 };
--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -5,7 +5,12 @@ const request = require('supertest'),
   Task = require('../src/models/task'),
   User = require('../src/models/user');
 
-const {testUser_1_id, testUser_1, setupDB} = require('./fixtures/db');
+const { testUser_1_id, testUser_1,
+        testUser_2_id, testUser_2,
+        task_1, task_2,
+        task_3, task_4,
+        task_5, task_6,
+      setupDB} = require('./fixtures/db');
 
 beforeEach(setupDB);
 
@@ -19,5 +24,24 @@ test('Should succeed@ creating a task as a user with auth', async()=>{
     .expect(201);
     const task = await Task.findById(response.body._id);
     expect(task).not.toBeNull();
-    expect(task.complete).toEqual(false); 
+    expect(task.complete).toEqual(false);
+});
+
+test('Should succeed@ getting the tasks for a specific user with auth', async()=>{
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${testUser_1.tokens[0].token}`)
+    .send({})
+    .expect(200);
+  expect(response.body.length).toEqual(4)
+});
+
+test('Should fail@ deleting other user tasks', async()=>{
+  const response = await request(app)
+    .delete(`/tasks/${task_1._id}`)
+    .set('Authorization', `Bearer ${testUser_2.tokens[0].token}`)
+    .send({})
+    .expect(404);
+  const task = await Task.findById(task_1._id);
+  expect (task).not.toBeNull(); 
 });
